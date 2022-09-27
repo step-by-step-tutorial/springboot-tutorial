fffb5ab96376d78fe55ec19236ab98a9d9a273d6
Saman Alishirishahrbabak <samanalishiri@gmail.com>
2022-09-28 00:24:07 +0200

    added step 001 as a hello world

---
+++ step-001-helloworld/README.md
@@ -0,0 +1,3 @@
+## Description
+
+This is the first step of the Spring Boot tutorial. The result is print `Hello World!`.
---
+++ step-001-helloworld/src/test/java/com/tutorial/springboot/helloworld/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tutorial.springboot.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void printMessage() {
+		System.out.println("Hello World Test");
+	}
+
+}
