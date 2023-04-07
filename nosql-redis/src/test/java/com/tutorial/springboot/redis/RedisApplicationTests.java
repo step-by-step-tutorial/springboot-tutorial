e1698b0141df15d32dc66b950e73697dca75cc3d
Saman Alishirishahrbabak <samanalishiri@gmail.com>
2023-04-07 22:36:36 +0200

    temp

---
+++ nosql-redis/src/test/java/com/tutorial/springboot/redis/RedisApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tutorial.springboot.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
