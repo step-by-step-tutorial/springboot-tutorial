4b337927802c2b8ba5b43bc72cbefcc14635328d
Saman Alishirishahrbabak <samanalishiri@gmail.com>
2025-01-14 21:12:31 +0100

    new update

--- cdc-debezium/src/test/java/com/tutorial/springboot/cdcdebezium/ApplicationTests.java
+++ cdc-debezium/src/test/java/com/tutorial/springboot/cdcdebezium/ApplicationTests.java
@@ -1,20 +1,61 @@
 package com.tutorial.springboot.cdcdebezium;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@ActiveProfiles({"test", "h2", "redis", "embedded-kafka"})
+@ActiveProfiles({"test", "mysql", "redis", "embedded-kafka"})
+@Testcontainers
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9094", "port=9094"})
-@DirtiesContext
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    static final Logger LOGGER = LoggerFactory.getLogger(ApplicationTests.class.getSimpleName());
+
+    @Container
+    static final MySQLContainer<?> MYSQL = new MySQLContainer<>("mysql:8.0");
+
+    static {
+        try {
+            MYSQL.withDatabaseName("test_db")
+                    .withUsername("user")
+                    .withPassword("password");
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    @SuppressWarnings({"unused"})
+    @DynamicPropertySource
+    static void registerMySQLProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MYSQL::getJdbcUrl);
+        registry.add("spring.datasource.username", MYSQL::getUsername);
+        registry.add("spring.datasource.password", MYSQL::getPassword);
+        registry.add("spring.datasource.driver-class-name", MYSQL::getDriverClassName);
+    }
+
+    static void start() {
+        MYSQL.start();
+    }
+
+    static void stop() {
+        MYSQL.stop();
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
