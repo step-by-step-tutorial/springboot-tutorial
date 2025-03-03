d28a57c59535abb1663ef24fc3a5d877228c4ca2
Saman Alishirishahrbabak <samanalishiri@gmail.com>
2025-03-03 17:47:26 +0100

    new update

--- messaging-redis/README.md
+++ messaging-redis/README.md
@@ -121,6 +121,12 @@ kubectl get all -n dev
 ```shell
 kubectl port-forward service/application 8080:8080 -n dev
 ```
+```shell
+kubectl port-forward service/redisinsight 5540:5540 -n dev
+```
+```shell
+kubectl port-forward service/commander 8081:8081 -n dev
+```
 
 ### E2eTest
 
--- messaging-redis/src/main/java/com/tutorial/springboot/messagingredis/config/RedisConfig.java
+++ messaging-redis/src/main/java/com/tutorial/springboot/messagingredis/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.tutorial.springboot.messagingredis.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.MessageListener;
@@ -14,9 +15,18 @@ import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new JedisConnectionFactory();
+        var connectionFactory = new JedisConnectionFactory();
+        connectionFactory.setHostName(host);
+        connectionFactory.setPort(port);
+        return connectionFactory;
     }
 
     @Bean
